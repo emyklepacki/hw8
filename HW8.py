1b392ad954088d4276899c23bbd96697bf470195
emyklepacki <123495101+emyklepacki@users.noreply.github.com>
2023-04-11 19:09:31 -0400

    Update HW8.py

--- HW8.py
+++ HW8.py
@@ -3,6 +3,7 @@
 # Your email: klepacki@umich.edu
 # List who you have worked with on this homework: no one
 
+import matplotlib
 import matplotlib.pyplot as plt
 import os
 import sqlite3
@@ -36,6 +37,7 @@ def load_rest_data(db):
         temp_d['rating'] = rating
         dct[i[0]] = temp_d
     # print(dct)
+
     return dct
 
 
@@ -53,12 +55,24 @@ def plot_rest_categories(db):
     cur.execute("SELECT category, id FROM categories")
     result = cur.fetchall()
     result = sorted(result)
+    count_list = []
+    cat_list = []
     for c in result:
         # print(c)
         cur.execute("SELECT count(*) FROM restaurants WHERE restaurants.category_id = ?", (c[1], ))
         count = cur.fetchall()
         dct[c[0]] = count[0][0]
-    # dct = sorted(dct, key = lambda t:t[0])
+        cat_list.append(c[0])
+        count_list.append(count[0][0])
+
+    fig,ax = plt.subplots()
+    ax.barh(cat_list, count_list)
+    ax.set_xlabel('count')
+    ax.set_ylabel('categories')
+    ax.set_title('restaurant categories x count')
+    ax.grid()
+    plt.show()
+
     return(dct)
 
 def find_rest_in_building(building_num, db):
@@ -67,7 +81,23 @@ def find_rest_in_building(building_num, db):
     restaurant names. You need to find all the restaurant names which are in the specific building. The restaurants 
     should be sorted by their rating from highest to lowest.
     '''
-    pass
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    dct = {}
+    
+    cur.execute("SELECT id FROM buildings WHERE building = ?", (building_num, ))
+    result = cur.fetchall()
+    b_id = result[0][0]
+
+    cur.execute("SELECT name, rating FROM restaurants WHERE building_id = ?", (b_id, ))
+    lst = cur.fetchall()
+    lst = sorted(lst, key = lambda t:t[1], reverse = True)
+    new_lst = []
+    for l in lst:
+        new_lst.append(l[0])
+
+    return new_lst
+
 
 #EXTRA CREDIT
 def get_highest_rating(db): #Do this through DB as well
@@ -124,11 +154,11 @@ class TestHW8(unittest.TestCase):
         self.assertEqual(cat_data, self.cat_dict)
         self.assertEqual(len(cat_data), 14)
 
-    # def test_find_rest_in_building(self):
-    #     restaurant_list = find_rest_in_building(1140, 'South_U_Restaurants.db')
-    #     self.assertIsInstance(restaurant_list, list)
-    #     self.assertEqual(len(restaurant_list), 3)
-    #     self.assertEqual(restaurant_list[0], 'BTB Burrito')
+    def test_find_rest_in_building(self):
+        restaurant_list = find_rest_in_building(1140, 'South_U_Restaurants.db')
+        self.assertIsInstance(restaurant_list, list)
+        self.assertEqual(len(restaurant_list), 3)
+        self.assertEqual(restaurant_list[0], 'BTB Burrito')
 
     # def test_get_highest_rating(self):
     #     highest_rating = get_highest_rating('South_U_Restaurants.db')
